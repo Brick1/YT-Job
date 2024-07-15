2b31e50d7dc33a0c03d7fc4d8c0f5b728e882189
VašQe <v.komarek@post.cz>
2024-07-15 17:38:39 +0200

    Rewritten the MediaConverter class since the FFMPEG.Net doesn't work anymore

--- YoutubeDownloaderLib/MediaConverter.cs
+++ YoutubeDownloaderLib/MediaConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace YoutubeDownloader
 {
     public class MediaConverter
     {
+        private static readonly string ffmpegPath = Path.Combine(Environment.CurrentDirectory, "ffmpeg.exe");
+
         /// <summary>
         /// Converts media file based on input path and output path
         /// </summary>
@@ -15,11 +18,59 @@ namespace YoutubeDownloader
         /// <param name="outputPath">Full path of the output file</param>
         public static async Task ConvertAsync(string inputPath, string outputPath, CancellationToken token)
         {
-            var input = new InputFile(inputPath);
-            var output = new OutputFile(outputPath);
-            var ffmpegPath = Path.Combine(Environment.CurrentDirectory, "ffmpeg.exe");
-            var ffmpeg = new Engine(ffmpegPath);
-            await ffmpeg.ConvertAsync(input, output, token);
+            string arguments = $"-i \"{inputPath}\" \"{outputPath}\"";
+            await ExecuteFFmpegCommand(arguments);
+        }
+
+        public static async Task<string> ExecuteFFmpegCommand(string arguments)
+        {
+            var tcs = new TaskCompletionSource<string>();
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ffmpegPath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (e.Data == null)
+                {
+                    tcs.TrySetResult("None data");
+                }
+                else
+                {
+                    tcs.TrySetResult(e.Data);
+                    Console.WriteLine(e.Data);
+                }
+            };
+
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data == null)
+                {
+                    tcs.TrySetResult("None data");
+                }
+                else
+                {
+                    tcs.TrySetResult("ERROR: " + e.Data);
+                    Console.WriteLine(e.Data);
+                }
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            await Task.Run(() => process.WaitForExit());
+            return await tcs.Task;
         }
     }
 }
