c77afc0ff14fa48a2577577d17a9a7803428586d
VašQe <v.komarek@post.cz>
2024-07-16 17:21:25 +0200

    Added test for HelperClass for the ExtractionFromURL method

--- UnitTests/HelperTests.cs
+++ UnitTests/HelperTests.cs
@@ -1,4 +1,5 @@
 using YoutubeDownloader.Youtube.Models;
+using static System.Net.WebRequestMethods;
 
 namespace UnitTests
 {
@@ -34,11 +35,32 @@ namespace UnitTests
                 "startsign"
             };
 
-            for(int i = 0; i < titles.Length; i++)
+            for (int i = 0; i < titles.Length; i++)
             {
                 string title = titles[i];
                 string actual = Helper.ValidateTitle(title);
                 Assert.That(actual, Is.EqualTo(excepted[i]));
+            }
+        }
+
+        [Test]
+        public void TestExtractionFromUrl()
+        {
+            List<Tuple<string, string, string>> tuples = new List<Tuple<string, string, string>>
+            {
+              new Tuple<string, string, string> ( "https://www.youtube.com/playlist?list=PLnPE367qhYpB8sfyp7UJxY6VJpd0BsFnX","PLnPE367qhYpB8sfyp7UJxY6VJpd0BsFnX","" ),
+              new Tuple<string, string, string> ( "https://www.youtube.com/watch?v=wh_qyX6D9wM&list=PLnPE367qhYpB8sfyp7UJxY6VJpd0BsFnX&index=1","PLnPE367qhYpB8sfyp7UJxY6VJpd0BsFnX","wh_qyX6D9wM" ),
+              new Tuple<string, string, string> ( "https://www.youtube.com/watch?v=wh_qyX6D9wM","","wh_qyX6D9wM" )
+            };
+
+            foreach (var item in tuples)
+            {
+                string url = item.Item1;
+                string actualPlaylistID = Helper.ExtractPlaylistID(url);
+                string actualVideoID = Helper.ExtractVideoID(url);
+
+                Assert.That(actualPlaylistID, Is.EqualTo(item.Item2));
+                Assert.That(actualVideoID, Is.EqualTo(item.Item3));
             }
         }
     }
